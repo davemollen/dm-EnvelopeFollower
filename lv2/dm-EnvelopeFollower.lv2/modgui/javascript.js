8c917d7c59dbb06827831105847c7f9a143a9b82
Dave Mollen <davemollen@gmail.com>
2026-06-15 15:13:33 +0200

    Add cleanup function

--- lv2/dm-EnvelopeFollower.lv2/modgui/javascript.js
+++ lv2/dm-EnvelopeFollower.lv2/modgui/javascript.js
@@ -1,33 +1,42 @@
 
 function(event) {
-  const canvas = event.icon.find("#scope")[0];
-  const ctx = canvas.getContext("2d");
-
   // initialize scope state
   if (!event.data.scope) {
     event.data.scope = {
       isEnabled: false,
       buffer: new Array(240).fill(0),
+      bufferIndex: 0,
       lastValue: 0,
       runningValue: 0,
-      smoothingFactor: 0.5 // decrease for more smoothing
+      smoothingFactor: 0.5, // decrease for more smoothing
+      animationFrameId: null,
+      canvas: null,
+      ctx: null,
     };
   }
 
   const scope = event.data.scope;
-  const buffer = scope.buffer;
-  const bufferSize = buffer.length;
+  if (!scope.canvas || !scope.ctx) {
+    scope.canvas = event.icon.find("#scope")[0];
+    scope.ctx = scope.canvas.getContext("2d");
+  }
+  const canvas = scope.canvas;
+  const ctx = scope.ctx;
 
   function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
 
-    for (let i = 0; i < buffer.length; i++) {
-      const x = (i / (buffer.length - 1)) * canvas.width;
-      const y = (1 - buffer[i]) * canvas.height;
+    for (let i = 0; i < scope.buffer.length; i++) {
+      const index = (scope.bufferIndex + i) % scope.buffer.length;
+      const x = (i / (scope.buffer.length - 1)) * canvas.width;
+      const y = (1 - scope.buffer[index]) * canvas.height;
 
-      if (i === 0) ctx.moveTo(x, y);
-      else ctx.lineTo(x, y);
+      if (i == 0) {
+        ctx.moveTo(x, y);
+      } else {
+        ctx.lineTo(x, y);
+      }
     }
 
     ctx.strokeStyle = "#FFD573";
@@ -43,12 +52,11 @@ function(event) {
     scope.runningValue =
       scope.runningValue + (scope.lastValue - scope.runningValue) * scope.smoothingFactor;
 
-    // push into buffer continuously
-    buffer.push(scope.runningValue);
-    buffer.shift();
+    scope.buffer[scope.bufferIndex] = scope.runningValue;
+    scope.bufferIndex = (scope.bufferIndex + 1) % scope.buffer.length;
 
     draw();
-    requestAnimationFrame(tick);
+    scope.animationFrameId = requestAnimationFrame(tick);
   }
 
   function startScope() {
@@ -60,6 +68,17 @@ function(event) {
     scope.isEnabled = false;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     scope.buffer.fill(0);
+  }
+
+  function cleanUp() {
+    scope.isEnabled = false;
+    if(scope.animationFrameId != null) {
+      cancelAnimationFrame(scope.animationFrameId);
+      scope.animationFrameId = null;
+    }
+    scope.canvas = null;
+    scope.ctx = null;
+    scope.buffer = null;
   }
 
   function handle_port_values(symbol, value) {
@@ -104,5 +123,7 @@ function(event) {
   }
   else if (event.type == 'change') {  
     handle_port_values(event.symbol, event.value);
+  } else if (event.type == 'end') {
+    cleanUp();
   }
 }
